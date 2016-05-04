3cb3bbf508eab266c54f76f4439cc450705b5c17
Wu <2206618839@qq.com>
2016-05-04 18:51:27 +0800

    工程建立

---
+++ WWHSlideController/Classes/WWHSlideController.h
@@ -0,0 +1,13 @@
+//
+//  WWHSlideController.h
+//  Pods
+//
+//  Created by Wu on 16/5/4.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWHSlideController : UINavigationController
+
+@end
---
+++ WWHSlideController/Classes/WWHSlideController.m
@@ -0,0 +1,37 @@
+//
+//  WWHSlideController.m
+//  Pods
+//
+//  Created by Wu on 16/5/4.
+//
+//
+
+#import "WWHSlideController.h"
+
+@interface WWHSlideController ()
+
+@end
+
+@implementation WWHSlideController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
